bf98c2e0ab5688aa5d14b440d96bd6566c4577ca
bosco <56211856+yang94pro@users.noreply.github.com>
2020-01-31 18:31:05 +0800

    lol

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 	let input;
 	import io from 'socket.io-client';
-	const socket = io('http://localhost');
+	const socket = io('http://127.0.0.1:5000');
 
 	import { beforeUpdate, afterUpdate } from 'svelte';
 
