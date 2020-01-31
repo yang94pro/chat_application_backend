a956f6a14bf0c821db17aa364f0cd3f05bb50d72
bosco <56211856+yang94pro@users.noreply.github.com>
2020-01-31 19:02:21 +0800

    yo

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-socketio = SocketIO(
-    app, 
-    cors_allowed_origins="*",)
+socketio = SocketIO(app,cors_allowed_origins="*") 
 
 @app.route("/")
 def sessions():
