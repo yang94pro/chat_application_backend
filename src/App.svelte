d8447b501b1f813db10d5177d3af5f63fe7a6490
bosco <56211856+yang94pro@users.noreply.github.com>
2020-02-04 17:20:54 +0800

    checkbox

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
 	let name  ;
 	let currentuser = name;
-
+	
 	let input;
 	import io from 'socket.io-client';
-	const socket = io('http://127.0.0.1:5000');
+	const socket = io('https://polar-eyrie-04595.herokuapp.com');
 
 	import { beforeUpdate, afterUpdate } from 'svelte';
 
@@ -21,14 +21,15 @@
 
 	
 	let comments = [
-		{ author: 'eliza', text:"how are you" }
+		{ author: 'eliza', text:"how are you", active:false}
 	];
 	socket.on('chat message', function (json) {
         comments = comments.concat({
 				author: json.author,
 				text: json.commend,
-				time: json.time
-			});;
+				time: json.time,
+				active: json.active,
+			});
       });
 
 
@@ -43,6 +44,8 @@
 				"author": name, 
 				"commend": text, 
 				"time": Date(),
+				"active":false,
+				
 			});
 			
 			console.log(comments);
@@ -67,10 +70,20 @@
 			{#if comment.author==="system"}
 				<span class="systemsg">{comment.text}</span>
 			{:else}
-				<article class="{comment.author=== name? "user":"other"}">
-					<span>{#if comment.author != name}{comment.author}:<n></n>{/if} {comment.text}</span>
-				</article>
-				{comment.time}
+				
+				<div>
+					<article class="{comment.author=== name? "user":"other"}">
+						<input type=checkbox id={comment} bind:checked={comment.active} >
+						<label for={comment}>{#if comment.author != name}{comment.author}:<n></n>{/if} {comment.text} </label>
+			
+					</article>
+					{#if comment.active}
+					<div class="receivedate">
+						{comment.time}
+					</div>
+					{/if}
+				</div>
+				
 			{/if}
 		{/each}
 	</div>
@@ -109,8 +122,11 @@
      
 	  font: 20px Helvetica, Arial;
     }
+	.default{
+		opacity: 0;
+	}
 
-	
+		
 	.systemsg{
 		text-align: center;
 		display: flex;
@@ -130,6 +146,7 @@
 	
 		
 	}
+
 
 	.chat {
 		position: relative;
@@ -153,7 +170,16 @@
 		word-wrap: break-word;
 		
 	}
+	div{
+		position: relative;
+	}
 
+	input[type="checkbox"] {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+		}
 	article {
 		margin: 0.5em 0;
 		
@@ -164,13 +190,14 @@
 
 	}
 
-	span {
+	label {
 		padding: 0.5em 1em;
 		display: inline-block;
+		cursor: pointer;
 	
 	}
 
-	.other span {
+	.other label {
 		background-color: #eee;
 		border-radius: 1em 1em 1em 0;
 		max-width: 250px;
@@ -178,7 +205,7 @@
 		
 	}
 
-	.user span {
+	.user label {
 		background-color: #0074D9;
 		color: white;
 		border-radius: 1em 1em 0 1em;
@@ -186,6 +213,17 @@
 		max-width: 250px;
 		word-wrap: break-word;
 	}
+
+
+	.receivedate{
+		opacity: 1;
+		text-align: center;
+		font-size: 15px;
+		color: rgb(138, 136, 136);
+		margin:0;
+		padding:0;
+	}
+
 
 	@media only screen and (max-width: 400px) {
 		.headdiv{
