0a4c96665b951c288612c577d60bd4a9e3d3d9be
bosco <56211856+yang94pro@users.noreply.github.com>
2020-02-08 17:39:58 +0800

    SKDJF

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from flask_cors import CORS
 import eventlet
 from bson import json_util
 
-
-client = MongoClient("mongodb+srv://ChatAdmin:123123123@chathistorydb-hwnzu.gcp.mongodb.net/test?retryWrites=true&w=majority")
-db = client.msgdatabase
-collection = db.chat_history
+try:
+    client = MongoClient("mongodb+srv://ChatAdmin:123123123@chathistorydb-hwnzu.gcp.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.msgdatabase
+    collection = db.chat_history
+except:
+    print("CANNNT CONNECT DATABASE MONGODB")
 
 app = Flask(__name__)
 CORS(app)
@@ -25,10 +27,12 @@ def chathistory (methods=['GET']):
 
 @socketio.on ('chat message')
 def resp (msg,methods=['GET','POST']):
+    print(msg)
     g=request.remote_addr
     retrmsg=json_util.dumps(msg)
     msg['ip']=g
-    collection.insert_one(msg)
+    try: collection.insert_one(msg)
+    except: print("CANNNT WRITE THE DATA INTO DATABASE")
     socketio.emit('chat message', retrmsg)
 
 if __name__ == "__main__":
